48470d7757f479c259a32ff4b69e8c1507bfdd04
Gabor Ballabas <gbr666@gmail.com>
2024-10-25 17:54:50 +0200

    Create migration for adding publisher to Book entity

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -33,6 +33,8 @@ public record Book(
     @Positive(message = "The book price must be greater than zero.")
     Double price,
 
+    String publisher,
+
     @CreatedDate
     Instant createdDate,
 
@@ -55,6 +57,7 @@ public record Book(
             title,
             author,
             price,
+            null,
             null,
             null,
             0
